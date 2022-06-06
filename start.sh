f64fc310fd20ac0e17d9a628e944499aef71cef5
mkAdmin11 <100592027+mkAdmin11@users.noreply.github.com>
2022-06-06 16:39:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt update
 sudo apt install -y ansible git
 
 sudo mkdir /ansible
 sudo chown -R $(whoami):$(whoami) /ansible/
-git clone git@github.com:mkAdmin11/ansible.git /ansible/
+git clone https://github.com/mkAdmin11/ansible.git /ansible/
 
 sudo rm -rf /etc/ansible/roles/
 sudo ln -s /ansible/roles/ /etc/ansible/
