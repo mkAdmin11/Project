cb1b2d2956b0d8bd1997734164573476709adf17
mkAdmin11 <100592027+mkAdmin11@users.noreply.github.com>
2022-06-09 16:48:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt update
 sudo apt install -y ansible git
 
-sudo mkdir /ansible
-sudo chown -R $(whoami):$(whoami) /ansible/
-git clone git@github.com:mkAdmin11/ansible.git
+mv /home/$(whoami)/.ssh/id_rsa /home/$(whoami)/.ssh/id_rsa_backup_$(date +%F_%T)
+vi /home/$(whoami)/.ssh/id_rsa
+mv /home/$(whoami)/.ssh/id_rsa.pub /home/$(whoami)/.ssh/id_rsa.pub_backup_$(date +%F_%T)
+vi /home/$(whoami)/.ssh/id_rsa.pud
+chmod 600 /home/$(whoami)/.ssh/id_rsa*
 
 git config --global user.name "Admin11SF"
 git config --global user.email "k-maksim@internet.ru"
 
-sudo mv /etc/ansible/roles/ /etc/ansible/backup_roles_$(date +%F_%T)
-sudo ln -s /ansible/roles/ /etc/ansible/
-sudo mv /etc/ansible/hosts /etc/ansible/backup_hosts_$(date +%F_%T)
-sudo ln -s /ansible/hosts /etc/ansible/
+sudo mkdir /ansible
+sudo chown -R $(whoami):$(whoami) /ansible/
+git clone git@github.com:mkAdmin11/ansible.git /ansible/
 
-vi /home/$(whoami)/.ssh/id_rsa
-vi /home/$(whoami)/.ssh/id_rsa.pud
-chmod 600 /home/$(whoami)/.ssh/id_rsa*
+sudo mv /etc/ansible/roles/ /etc/ansible/roles_backup_$(date +%F_%T)
+sudo ln -s /ansible/roles/ /etc/ansible/
+sudo mv /etc/ansible/hosts /etc/ansible/hosts_backup_$(date +%F_%T)
+sudo ln -s /ansible/hosts /etc/ansible/
