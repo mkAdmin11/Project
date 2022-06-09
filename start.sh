7848f1a9ff2744f7d0c5cec653c0f031c174015c
mkAdmin11 <100592027+mkAdmin11@users.noreply.github.com>
2022-06-09 16:32:41 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,10 @@ sudo apt install -y ansible git
 
 sudo mkdir /ansible
 sudo chown -R $(whoami):$(whoami) /ansible/
-git clone https://github.com/mkAdmin11/ansible.git /ansible/
+git clone git@github.com:mkAdmin11/ansible.git
+
+git config --global user.name "Admin11SF"
+git config --global user.email "k-maksim@internet.ru"
 
 sudo mv /etc/ansible/roles/ /etc/ansible/backup_roles_$(date +%F_%T)
 sudo ln -s /ansible/roles/ /etc/ansible/
