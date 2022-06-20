a2da18800ac4f3b335839932c8b770192e44576a
Admin11SF <k-maksim@internet.ru>
2022-06-20 17:10:47 +0300

    .

--- start_ssh.sh
+++ start_ssh.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 mv /home/$(whoami)/.ssh/id_rsa /home/$(whoami)/.ssh/id_rsa_backup_$(date +%F_%T)
+echo -e "/nРедактировать приватный ключ (нажать Enter)"
 vi /home/$(whoami)/.ssh/id_rsa
+
 mv /home/$(whoami)/.ssh/id_rsa.pub /home/$(whoami)/.ssh/id_rsa.pub_backup_$(date +%F_%T)
+echo -e "/nРедактировать публичный ключ (нажать Enter)"
 vi /home/$(whoami)/.ssh/id_rsa.pud
+
 chmod 600 /home/$(whoami)/.ssh/id_rsa*
 
-echo "IP первого сервера:"
+echo -e "/nIP первого сервера:"
 read IP_1
 ssh-keyscan -H $(IP_1) >> ~/.ssh/known_hosts
 
-echo "IP второго сервера:"
+echo -e "/nIP второго сервера:"
 read IP_2
 ssh-keyscan -H $(IP_2) >> ~/.ssh/known_hosts
