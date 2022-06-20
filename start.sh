7ecbfef0adff32731d2b63a12fa899d80cd49dc8
Admin11SF <k-maksim@internet.ru>
2022-06-20 18:05:41 +0300

    .

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Приступаем"
+
 sudo apt-add-repository -y ppa:ansible/ansible > /dev/null 2>&1
 echo "Добавлен репозиторий Ansible"
 
@@ -15,19 +17,20 @@ mv /home/$(whoami)/.ssh/id_rsa.pub /home/$(whoami)/.ssh/id_rsa.pub_backup_$(date
 echo "Редактировать публичный ключ (нажать Enter)"
 read zero
 vi /home/$(whoami)/.ssh/id_rsa.pud
+
 chmod 600 /home/$(whoami)/.ssh/id_rsa*
 
 echo "IP первого сервера:"
 read IP_1
-ssh-keyscan -H $IP_1 >> ~/.ssh/known_hosts
+ssh-keyscan -H $IP_1 >> ~/.ssh/known_hosts  > /dev/null 2>&1
 
 echo "IP второго сервера:"
 read IP_3
-ssh-keyscan -H $IP_2 >> ~/.ssh/known_hosts
+ssh-keyscan -H $IP_2 >> ~/.ssh/known_hosts  > /dev/null 2>&1
 
 echo "IP третьего сервера:"
 read IP_3
-ssh-keyscan -H $IP_3 >> ~/.ssh/known_hosts
+ssh-keyscan -H $IP_3 >> ~/.ssh/known_hosts  > /dev/null 2>&1
 
 git config --global user.name "Admin11SF"  > /dev/null 2>&1
 git config --global user.email "k-maksim@internet.ru"  > /dev/null 2>&1
@@ -38,9 +41,9 @@ sudo chown -R $(whoami):$(whoami) /ansible/   > /dev/null 2>&1
 git clone git@github.com:mkAdmin11/ansible.git /ansible/   > /dev/null 2>&1
 echo "Склонирован Git репозиторий mkAdmin11/ansible"
 
-sudo mv /etc/ansible/roles/ /etc/ansible/roles_backup_$(date +%F_%T)
+sudo mv /etc/ansible/roles/ /etc/ansible/roles_backup_$(date +%F_%T) 2> /dev/null
 sudo ln -s /ansible/roles/ /etc/ansible/
-sudo mv /etc/ansible/hosts /etc/ansible/hosts_backup_$(date +%F_%T)
+sudo mv /etc/ansible/hosts /etc/ansible/hosts_backup_$(date +%F_%T) 2> /dev/null
 sudo ln -s /ansible/hosts /etc/ansible/
 echo "Настроен Ansible"
 
