214d34980198df75930f17c0bc585241efc76ebf
mkAdmin11 <100592027+mkAdmin11@users.noreply.github.com>
2022-06-21 11:07:24 +0300

    Update start_ssh.sh

--- start_ssh.sh
+++ start_ssh.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mv /home/$(whoami)/.ssh/id_rsa /home/$(whoami)/.ssh/id_rsa_backup_$(date +%F_%T) 2> /dev/null
+mv /home/$(whoami)/.ssh/id_rsa /home/$(whoami)/.ssh/id_rsa_backup_$(date +%F_%T)
 echo "Редактировать приватный ключ (нажать Enter)"
 read zero
 vi /home/$(whoami)/.ssh/id_rsa
 
-mv /home/$(whoami)/.ssh/id_rsa.pub /home/$(whoami)/.ssh/id_rsa.pub_backup_$(date +%F_%T) 2> /dev/null
+mv /home/$(whoami)/.ssh/id_rsa.pub /home/$(whoami)/.ssh/id_rsa.pub_backup_$(date +%F_%T)
 echo "Редактировать публичный ключ (нажать Enter)"
 read zero
 vi /home/$(whoami)/.ssh/id_rsa.pud
@@ -14,8 +14,8 @@ chmod 600 /home/$(whoami)/.ssh/id_rsa*
 
 echo "IP первого сервера:"
 read IP_1
-ssh-keyscan -H $IP_1 >> ~/.ssh/known_hosts  2> /dev/null
+ssh-keyscan -H $IP_1 >> ~/.ssh/known_hosts
 
 echo "IP третьего сервера:"
 read IP_3
-ssh-keyscan -H $IP_3 >> ~/.ssh/known_hosts  2> /dev/null
+ssh-keyscan -H $IP_3 >> ~/.ssh/known_hosts
