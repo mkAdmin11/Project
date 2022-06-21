46c7673cefe02fa5470583fcb63e0d47695cd1eb
mkAdmin11 <100592027+mkAdmin11@users.noreply.github.com>
2022-06-21 11:09:36 +0300

    Update start_ssh.sh

--- start_ssh.sh
+++ start_ssh.sh
@@ -4,18 +4,22 @@ mv /home/$(whoami)/.ssh/id_rsa /home/$(whoami)/.ssh/id_rsa_backup_$(date +%F_%T)
 echo "Редактировать приватный ключ (нажать Enter)"
 read zero
 vi /home/$(whoami)/.ssh/id_rsa
+echo "========================================================================"
 
 mv /home/$(whoami)/.ssh/id_rsa.pub /home/$(whoami)/.ssh/id_rsa.pub_backup_$(date +%F_%T)
 echo "Редактировать публичный ключ (нажать Enter)"
 read zero
 vi /home/$(whoami)/.ssh/id_rsa.pud
+echo "========================================================================"
 
 chmod 600 /home/$(whoami)/.ssh/id_rsa*
 
 echo "IP первого сервера:"
 read IP_1
 ssh-keyscan -H $IP_1 >> ~/.ssh/known_hosts
+echo "========================================================================"
 
 echo "IP третьего сервера:"
 read IP_3
 ssh-keyscan -H $IP_3 >> ~/.ssh/known_hosts
+echo "========================================================================"
